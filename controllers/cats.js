7227e672aa911bf9263bc794b51f74a53f7ae4b6
mzsellem-GA <125702378+mzsellem-GA@users.noreply.github.com>
2023-05-16 12:23:06 -0400

    delete functionality on cats and home index background

--- controllers/cats.js
+++ controllers/cats.js
@@ -10,8 +10,18 @@ function addCatToCustomer(req, res, next) {
         .catch(next)
 }
 
-
+function deleteCatFromCustomer(req, res, next) {
+    Customer.findById(req.params.customerId)
+        .then(customer => {
+            console.log('this is customer in deleteCat', customer)
+            customer.cats.id(req.params.catId).deleteOne()
+            return customer.save()
+        })
+        .then(() => res.redirect(`/customers/${req.params.customerId}`))
+        .catch(next)
+}
 
 module.exports = {
     addCatToCustomer,
+    deleteCatFromCustomer
 }
--- routes/cats.js
+++ routes/cats.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
 const catCtrl = require('../controllers/cats')
 
 router.post('/customers/:customerId', catCtrl.addCatToCustomer)
-
+router.delete('/:customerId/:catId', catCtrl.deleteCatFromCustomer)
 
 module.exports = router;
